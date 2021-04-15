3dd4e3afe182d8fe46737c32895d6c126cbc59a1
Emilio Peláez <me@epelaez.net>
2021-04-15 09:11:57 +0200

    Create UIKit Example project

---
+++ Example/Example-UIKit/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  Example
+//
+//  Created by Emilio Peláez on 15/4/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+	
+	var window: UIWindow?
+	
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let _ = (scene as? UIWindowScene) else { return }
+	}
+}
---
+++ Example/Example-UIKit/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Emilio Peláez on 15/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+}
