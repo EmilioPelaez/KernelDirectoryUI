f0648edc49c6e01ef5cd5e65c33f8dcb1b8812e4
Emilio Peláez <me@epelaez.net>
2021-04-15 23:16:34 +0200

    Remove unused methods

--- Sources/KernelDirectoryUI/Utilities/Publisher.swift
+++ Sources/KernelDirectoryUI/Utilities/Publisher.swift
@@ -8,41 +8,6 @@ import Foundation
 
 extension Publisher {
 	
-	func peek(watcher: @escaping (Output) -> Void) -> Publishers.HandleEvents<Self> {
-		handleOutput(handler: watcher)
-	}
-	
-	func peekError(watcher: @escaping (Failure) -> Void) -> Publishers.MapError<Self, Failure> {
-		mapError {
-			watcher($0)
-			return $0
-		}
-	}
-	
-	var didSet: Publishers.Delay<Self, RunLoop> {
-		delay(for: 0, scheduler: .main)
-	}
-	
-	func handleSubscription(handler: @escaping (Subscription) -> Void) -> Publishers.HandleEvents<Self> {
-		handleEvents(receiveSubscription: handler)
-	}
-	
-	func handleOutput(handler: @escaping (Output) -> Void) -> Publishers.HandleEvents<Self> {
-		handleEvents(receiveOutput: handler)
-	}
-	
-	func handleCompletion(handler: @escaping (Subscribers.Completion<Failure>) -> Void) -> Publishers.HandleEvents<Self> {
-		handleEvents(receiveCompletion: handler)
-	}
-	
-	func handleCancel(handler: @escaping () -> Void) -> Publishers.HandleEvents<Self> {
-		handleEvents(receiveCancel: handler)
-	}
-	
-	func handleRequest(handler: @escaping (Subscribers.Demand) -> Void) -> Publishers.HandleEvents<Self> {
-		handleEvents(receiveRequest: handler)
-	}
-	
 	func result(handler: @escaping (Result<Output, Failure>) -> Void) -> AnyCancellable {
 		sink {
 			guard case let .failure(error) = $0 else { return }
